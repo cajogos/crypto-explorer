72b6dbb44f11614404c7b49f27e38dc58c8b38f1
Carlos Ferreira <carlosferreira@outlook.com>
2018-05-15 23:10:14 +0100

    Reformated model objects

--- models/markets.js
+++ models/markets.js
@@ -1,13 +1,29 @@
-var mongoose = require('mongoose')
-  , Schema = mongoose.Schema;
- 
+var mongoose = require('mongoose'), Schema = mongoose.Schema;
+
 var MarketsSchema = new Schema({
-  market: { type: String },
-  summary: { type: Object, default: {} },
-  chartdata: { type: Array, default: [] },
-  buys: { type: Array, default: [] },
-  sells: { type: Array, default: [] },
-  history: { type: Array, default: [] },
+  market: {
+    type: String
+  },
+  summary: {
+    type: Object,
+    default: {}
+  },
+  chartdata: {
+    type: Array,
+    default: []
+  },
+  buys: {
+    type: Array,
+    default: []
+  },
+  sells: {
+    type: Array,
+    default: []
+  },
+  history: {
+    type: Array,
+    default: []
+  }
 });
 
 module.exports = mongoose.model('Markets', MarketsSchema);
--- models/tx.js
+++ models/tx.js
@@ -1,14 +1,35 @@
-var mongoose = require('mongoose')
-  , Schema = mongoose.Schema;
- 
+var mongoose = require('mongoose'), Schema = mongoose.Schema;
+
 var TxSchema = new Schema({
-  txid: { type: String, lowercase: true, unique: true, index: true},
-  vin: { type: Array, default: [] },
-  vout: { type: Array, default: [] },
-  total: { type: Number, default: 0 },
-  timestamp: { type: Number, default: 0 },
-  blockhash: { type: String },
-  blockindex: {type: Number, default: 0},
-}, {id: false});
+  txid: {
+    type: String,
+    lowercase: true,
+    unique: true,
+    index: true
+  },
+  vin: {
+    type: Array,
+    default: []
+  },
+  vout: {
+    type: Array,
+    default: []
+  },
+  total: {
+    type: Number,
+    default: 0
+  },
+  timestamp: {
+    type: Number,
+    default: 0
+  },
+  blockhash: {
+    type: String
+  },
+  blockindex: {
+    type: Number,
+    default: 0
+  }
+}, { id: false });
 
 module.exports = mongoose.model('Tx', TxSchema);
